a1e68e93162c2977de39da85a483e5a1c05d3a5e
Jacob Adams <jacob@tookmund.com>
2019-05-16 00:10:32 -0400

    newshare: Build windows script

--- newshare.sh
+++ newshare.sh
@@ -9,9 +9,12 @@ fi
 
 ssh-keygen -C "$1" -f "$1" -N ""
 
-FILENAME="$1"-filesync.command
-cat remhost filesync.sh.start "$1" filesync.sh.end > "$FILENAME"
-chmod +x "$FILENAME"
+FILENAME="$1"-filesync
+sed s/^/$/g remhost > remhost.ps1
+cat remhost filesync.sh.start "$1" filesync.sh.end > "$FILENAME.command"
+cat remhost.ps1 filesync.ps1.start "$1" filesync.ps1.end > "$FILENAME.ps1"
+chmod +x "$FILENAME.command"
+chmod +x "$FILENAME.ps1"
 
 ssh-copy-id -i "$1" "$REMUSER"@"$REMHOST"
 
